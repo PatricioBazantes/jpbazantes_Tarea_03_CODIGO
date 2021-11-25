cd3ed997d94f4496c15b823684bef9e8d6338866
PatricioBazantes <56976598+PatricioBazantes@users.noreply.github.com>
2021-11-25 01:47:16 -0500

    Update validar.js

--- js/validar.js
+++ js/validar.js
@@ -20,7 +20,7 @@ function validar() {
 		return false;
 	}else {
 		alert("Datos enviados correctamente");
-		document.getElementById("login").action="https://www.facebook.com/";
+		document.getElementById("login").action="https://patriciobazantes.github.io/jpbazantes_Tarea_03_CODIGO/index.html";
 	}
 }
 function checkSubmit() {
